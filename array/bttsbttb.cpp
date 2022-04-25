05ea512862c48257de25e039a814b26f88a3af6f
chiragV1 <chiragverma525@gmail.com>
2022-04-25 17:00:11 +0530

    duplicate using xor

---
+++ array/bttsbttb.cpp
@@ -0,0 +1,29 @@
+#include<cstdio>
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+class solution{
+
+public:
+int profit(vector<int> cost){
+
+int max= INT32_MAX;
+int min = INT32_MIN;
+
+
+for(int i =0;)
+
+return p;
+}
+
+
+
+};
+int main(){
+
+
+    return 0;
+}
--- array/duplicat_element_in_array.cpp
+++ array/duplicat_element_in_array.cpp
@@ -1,51 +1,85 @@
-#include<cstdio>
-#include<iostream>
+// #include<cstdio>
+// #include<iostream>
+
+// using namespace std;
+
+// class Solution{
+// public:
+//     int remove_duplicate(int a[],int n){
+//         // code here
+//         int arr[n];
+        
+//         if(n==0||n==1){
+//             return n;
+//         }
+//         int j=0;
+//         for(int i=0;i<n-1;i++){
+//             if(a[i]!=a[i+1]){
+//                 arr[j]=a[i];
+//                 j++;
+//             }
+//         }
+//         arr[j++]=a[n-1];
+        
+//         for(int i=0;i<j;i++){
+//             a[i]=arr[i];
+//         }
+        
+//         return j;
+//     }
+// };
+
+// int main(){
+//      int T;
+//      cin>>T;
+//      while(T--){
+//          Solution ob;
+//          int N;
+//          cin>>N;
+//          int a[N];
+//          for(int i=0;i<N;i++){
+//              cin>>a[i];
+//          }
+
+//          int n = ob.remove_duplicate(a, N);
+//          for(int i=0;i<n;i++){
+//              cout<<a[i]<<" ";
+//          }
+
+// }
+//     return 0;
+// }
+
+
+
+#include<bits/stdc++.h>
 
 using namespace std;
 
-class Solution{
-public:
-    int remove_duplicate(int a[],int n){
-        // code here
-        int arr[n];
-        
-        if(n==0||n==1){
-            return n;
-        }
-        int j=0;
-        for(int i=0;i<n-1;i++){
-            if(a[i]!=a[i+1]){
-                arr[j]=a[i];
-                j++;
-            }
-        }
-        arr[j++]=a[n-1];
-        
-        for(int i=0;i<j;i++){
-            a[i]=arr[i];
-        }
-        
-        return j;
+int main (){
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
-};
 
-int main(){
-     int T;
-     cin>>T;
-     while(T--){
-         Solution ob;
-         int N;
-         cin>>N;
-         int a[N];
-         for(int i=0;i<N;i++){
-             cin>>a[i];
-         }
+    //xor
+int ans =0;
+    for(int i=0;i<n;i++){
+ans = ans^arr[i];
+    }
 
-         int n = ob.remove_duplicate(a, N);
-         for(int i=0;i<n;i++){
-             cout<<a[i]<<" ";
-         }
+    //xor with numbers
 
-}
+    for(int i=0;i<n;i++){
+        ans = ans^i;
+    }
+
+    cout<<ans;
+
+
+
     return 0;
 }
