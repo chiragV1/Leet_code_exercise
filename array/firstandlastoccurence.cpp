bc35b808cb6ce3d834b6e8bf1d3b3785d49e919e
chiragV1 <chiragverma525@gmail.com>
2022-06-01 00:18:55 +0530

    ff

--- array/firstandlastoccurence.cpp
+++ array/firstandlastoccurence.cpp
@@ -1,9 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class solution{
 
-    public:
       int first(int arr[], int key ){
           int s=0;
           int e  = 5;
@@ -29,15 +27,15 @@ class solution{
    
 
 
-};
+
 
 int main(){
 
     int  arr[6] ={1,2,3,3,4,5};
     int key ;
     cin>>key;
-    solution ob;
-    int right = ob.first(arr, key);
+    
+    int right = first(arr, key);
 
     cout<<right;
 
